43eb5d5cec45a9a6e98e001c383ddf7ba236be62
emmanuel-bui <manuche@tuta.io>
2025-10-16 08:47:23 +0200

    Boundary ajoutervillageois complété

--- src/frontiere/BoundaryEmmenager.java
+++ src/frontiere/BoundaryEmmenager.java
@@ -11,28 +11,42 @@ public class BoundaryEmmenager {
 
 	public void emmenager(String nomVisiteur) {
 		if (controlEmmenager.isHabitant(nomVisiteur)) {
-			System.out.println(
-					"Mais vous êtes déjà un habitant du village !");
+			System.out.println("Mais vous êtes déjà un habitant du village !");
 		} else {
 			StringBuilder question = new StringBuilder();
 			question.append("Êtes-vous :\n");
 			question.append("1 - un druide.\n");
 			question.append("2 - un gaulois.\n");
+
 			int choixUtilisateur = -1;
 			do {
 				choixUtilisateur = Clavier.entrerEntier(question.toString());
 				switch (choixUtilisateur) {
 				case 1:
 					emmenagerDruide(nomVisiteur);
+					System.out.println("Bonjour villageois " + nomVisiteur);
+					int forceDruide = Clavier.entrerEntier("Quelle est votre force?");
+					int effetPotionMax = 0;
+					int effetPotionMin = 1;
+					while (effetPotionMax < effetPotionMin) {
+						effetPotionMin = Clavier
+								.entrerEntier("Quelle est la force de potion la plus faible que vous produisez ? ");
+						effetPotionMax = Clavier
+								.entrerEntier("Quelle est la force de potion la plus forte que vous produisez ? ");
+						if (effetPotionMax < effetPotionMin) {
+							System.out
+									.println("Attention druide, vous vous êtes trompé entre le minimum et le maximum");
+						}
+					}
+					controlEmmenager.ajouterDruide(nomVisiteur, forceDruide, effetPotionMin, effetPotionMax);
 					break;
-
 				case 2:
-					//TODO a completer
+					System.out.println("Bonjour villageois " + nomVisiteur);
+					int force = Clavier.entrerEntier("Quelle est votre force?");
+					controlEmmenager.ajouterGaulois(nomVisiteur, force);
 					break;
-
 				default:
-					System.out
-							.println("Vous devez choisir le chiffre 1 ou 2 !");
+					System.out.println("Vous devez choisir le chiffre 1 ou 2 !");
 					break;
 				}
 			} while (choixUtilisateur != 1 && choixUtilisateur != 2);
@@ -40,6 +54,6 @@ public class BoundaryEmmenager {
 	}
 
 	private void emmenagerDruide(String nomVisiteur) {
-		//TODO a completer
+
 	}
 }
